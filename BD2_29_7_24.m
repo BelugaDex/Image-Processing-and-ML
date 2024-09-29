305b23b1984184031a7adfe87847c6b8344f852d
BelugaDex <153344212+BelugaDex@users.noreply.github.com>
2024-09-29 20:12:25 +0530

    Update BD2_29_7_24.m

--- BD2_29_7_24.m
+++ BD2_29_7_24.m
@@ -1,4 +1,4 @@
-I = imread('C:/Users/BiswarajDey/OneDrive/Documents/Biswa VS CODE/Matlab Projects/Images/waterlilies.png');
+I = imread('C:/Users//waterlilies.png');
 imshow(I); 
 size(I);
 I2 =  imresize(I, 2) ;
