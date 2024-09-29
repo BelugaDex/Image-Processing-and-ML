5af385a82add2650c297b32f79edddf32ef3564c
BelugaDex <153344212+BelugaDex@users.noreply.github.com>
2024-09-29 20:14:14 +0530

    Update BD6_29_7_24.m

--- BD6_29_7_24.m
+++ BD6_29_7_24.m
@@ -1,4 +1,4 @@
-I = imread('C:/Users/BiswarajDey/OneDrive/Documents/Biswa VS CODE/Matlab Projects/Images/peppers.jpg');
+I = imread('C:/Users/peppers.jpg');
 figure,imshow(I); 
 f = 1/25*ones(5,5);  
 I2 = imfilter(I, f); 
